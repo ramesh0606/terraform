9faec703dc6c6a983536b19147495c91b5e0ac67
ramesh0606 <rameshsanapathi@gmail.com>
2024-04-21 01:51:32 +0530

    updated jenkins file

--- variables.tf
+++ variables.tf
@@ -10,3 +10,9 @@ variable "secret_key_var" {
   default = "tnaQnogT0BqYj1DnzphqzydwqCJ9cTji/prrRaNn"
 
 }
+
+variable "secret_123" {
+  type    = string
+  #default = "tnaQnogT0BqYj1DnzphqzydwqCJ9cTji/prrRaNn"
+
+}
